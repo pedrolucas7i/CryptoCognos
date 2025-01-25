81979c99ef2b83b1f3bece7b35fae5f665a31084
pedrolucas7i <pedrolucas.core7i@gmail.com>
2025-01-25 19:15:36 +0000

    Updates in Layout

--- dbPool.py
+++ dbPool.py
@@ -27,14 +27,14 @@ def connect(mariadb=None):
                             pool_size=10,
                             max_overflow=15,
                             pool_timeout=60,
-                            pool_recycle=1800)
+                            pool_recycle=18)
     elif mariadb is True:
         engine = create_engine(
                             f"mariadb+mariadbconnector://{user}:{password}@{host}/{database}?charset=utf8mb4",
                             pool_size=10,
                             max_overflow=15,
                             pool_timeout=60,
-                            pool_recycle=1800)
+                            pool_recycle=18)
     else:
         print("\033[1;34mSet the dbPool.connect() mariadb var to True if is mariaDB, if is a MySQL set to False")
         engine = create_engine(
@@ -42,7 +42,7 @@ def connect(mariadb=None):
                             pool_size=10,
                             max_overflow=15,
                             pool_timeout=60,
-                            pool_recycle=1800)
+                            pool_recycle=18)
         
 def get_engine():
     global engine
